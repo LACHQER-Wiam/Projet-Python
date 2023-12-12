89d9f2f365a4342173b95815052a717eae679f11
John Doe <wiamlachqer123@gmail.com>
2023-12-12 11:24:25 +0100

    exploration de la consommation

--- eda.py
+++ eda.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import seaborn as sns
+import matplotlib.pyplot as plt
 
 def correlation(df):
     corr = df.corr()
@@ -13,23 +14,34 @@ def scatterplot(data,x,y,var):
 def histplot(data,x,bins=30,binwidth=3):
     sns.histplot(data=data, x=x, bins=bins, binwidth=binwidth)
 
+#def lineplot(data, colonnes):
+     
+def boxplot(data, variables):
+    fig, axes = plt.subplots(nrows=2, ncols=3, figsize=(15, 8))
+    axes = axes.flatten()
+    for i in range(len(variables)):
+        sns.boxplot(x=data[variables[i]], ax=axes[i], color='skyblue')
+        #axes[i].set_title(variables[i])
+    # Masquer le subplot vide 
+    if len(axes) > 5:
+        for i in range(5, len(axes)):
+            fig.delaxes(axes[i])
+    plt.tight_layout()
+    plt.show()
+
+
+
 #Fonction qui permet d’explorer les données
 def create_unique(df):       
     df_unique = pd.DataFrame(columns=['Column_name','Data_type', 'Number_of_unique','Number_of_missing', 'Unique_values'])
-    # loop through the columns in the other dataframe
     for col in df.columns:
-        # get the number of unique values in the column
         num_unique = df[col].nunique()
-        # add the unique values as a list to the 'Unique_values' column if num_unique <= 5
         if num_unique <= 15:
             unique_vals = list(df[col].unique())
         else:
             unique_vals = "More than 15 unique values"
-        # get the data type of the column
         data_type = df[col].dtype
-        # count the number of missing values in the column
         num_missing = df[col].isnull().sum()
-        # append a row to the empty dataframe with the column name, number of unique values, unique values, and data type
         df_unique = pd.concat([df_unique,pd.DataFrame([{'Column_name': col, 'Number_of_unique': num_unique, 'Unique_values': unique_vals, 'Data_type':
                                       data_type, 'Number_of_missing': num_missing}])])
     return df_unique
